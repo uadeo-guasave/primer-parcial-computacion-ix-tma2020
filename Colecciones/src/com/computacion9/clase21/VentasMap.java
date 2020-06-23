6c44022198f3f54b5da813aa95d8f643f118eb5e
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-06-23 11:34:37 -0600

    Clase 21: Mapas
    HashMap
    - TODO: Mapa para controlar toda la estructura propuesta en clase.

---
+++ Colecciones/src/com/computacion9/clase21/VentasMap.java
@@ -0,0 +1,22 @@
+package com.computacion9.clase21;
+
+public class VentasMap {
+
+	public static void main(String[] args) {
+//		Venta ID: 9876, Fecha: 23/06/2020, Cliente: Bidkar
+//		Producto: Audifonos Sony WX, Cantidad: 1, Precio: 3000, Total: 3000
+//		Producto: Audifonos Sony MDR, Cantidad: 1, Precio: 600, Total: 600
+//
+//		Ventas
+//			Clientes
+//			Ventas_Productos
+//				Productos
+//					Categoria
+//
+//		HashMap<Venta, Cliente>
+//		HashMap<Venta, HashSet<VentaProducto>>
+//		HashMap<
+
+	}
+
+}
